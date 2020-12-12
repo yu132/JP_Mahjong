678de473688aacbceb6e98a57c92319f62fcc00d
yudingbang <876633022@qq,com>
2020-12-12 19:51:38 +0800

    役牌绿和役牌红分析代码

---
+++ JP Mahjong/src/main/java/yu/proj/ref/gameLogicChain/game/shared/analyze/yaku/dragon/AnalyzeRedTriplet.java
@@ -0,0 +1,32 @@
+package yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.dragon;
+
+import static yu.proj.ref.tile.TileType.*;
+
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.AnalyzeTriplet;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.YakuAnalyzeData;
+import yu.proj.ref.tile.TileType;
+import yu.proj.ref.tile.Yaku;
+
+/**  
+ * @ClassName: White  
+ *
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ *
+ * @author 余定邦  
+ *
+ * @date 2020年12月12日  
+ *  
+ */
+public class AnalyzeRedTriplet extends AnalyzeTriplet {
+
+    @Override
+    protected TileType getTripletType(YakuAnalyzeData data) {
+        return RED;
+    }
+
+    @Override
+    protected Yaku getYaku(YakuAnalyzeData data) {
+        return Yaku.DRAGON_R;
+    }
+
+}
---
+++ JP Mahjong/src/test/java/yu/proj/ref/gameLogicChain/game/shared/analyze/yaku/dragon/TestAnalyzeRedTriplet.java
@@ -0,0 +1,27 @@
+package yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.dragon;
+
+import static yu.proj.ref.tile.TileType.*;
+
+import org.junit.Test;
+
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.TestTripletUtil;
+import yu.proj.ref.tile.Yaku;
+
+/**  
+ * @ClassName: TestAnalyzePrevalentWindTriplet  
+ *
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ *
+ * @author 余定邦  
+ *
+ * @date 2020年11月28日  
+ *  
+ */
+public class TestAnalyzeRedTriplet {
+
+    @Test
+    public void test() {
+        new TestTripletUtil(RED, Yaku.DRAGON_R, new AnalyzeRedTriplet()).test();
+    }
+
+}

e81b0deafb233bc224ce7070cc01dfe53f0fde69
yu132 <876633022@qq.com>
2020-12-14 12:24:06 +0800

    小三元分析逻辑

---
+++ JP Mahjong/src/main/java/yu/proj/ref/gameLogicChain/game/shared/analyze/yaku/dragon/AnalyzeLittleThreeDragons.java
@@ -0,0 +1,24 @@
+package yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.dragon;
+
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.SimpleConditionYakuAnalyzer;
+import yu.proj.ref.tile.Yaku;
+
+/**  
+ * @ClassName: AnalyzeBigThreeDragons  
+ *
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ *
+ * @author 余定邦  
+ *
+ * @date 2020年12月13日  
+ *  
+ */
+public class AnalyzeLittleThreeDragons extends SimpleConditionYakuAnalyzer {
+
+    private final static int NUM_OF_LITTLE_THREE_DRAGON = 3 * 2 + 2;// 包括三元牌的两个刻子和一个对子
+
+    protected AnalyzeLittleThreeDragons() {
+        super((util) -> util.countDragons() == NUM_OF_LITTLE_THREE_DRAGON, Yaku.LITTLE_THREE_DRAGONS);
+    }
+
+}
---
+++ JP Mahjong/src/test/java/yu/proj/ref/gameLogicChain/game/shared/analyze/yaku/dragon/TestAnalyzeLittleThreeDragons.java
@@ -0,0 +1,54 @@
+package yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.dragon;
+
+import static org.junit.Assert.*;
+import static yu.proj.ref.tile.TileType.*;
+
+import org.junit.Test;
+
+import yu.proj.ref.gameLogicChain.game.shared.analyze.TestAnalyzeData;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.NoYaku;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.YakuAnalyzeData;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.YakuAnalyzer;
+import yu.proj.ref.tile.Yaku;
+
+/**  
+ * @ClassName: TestAnalyzeBigThreeDragons  
+ *
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ *
+ * @author 余定邦  
+ *
+ * @date 2020年12月13日  
+ *  
+ */
+public class TestAnalyzeLittleThreeDragons {
+
+    private TestAnalyzeData taData = new TestAnalyzeData();
+
+    private YakuAnalyzer analyzer = new AnalyzeLittleThreeDragons();
+
+    @Test
+    public void noYaku() {
+        new NoYaku().test(analyzer);
+    }
+
+    @Test
+    public void test() {
+        taData.draw(RED, RED, RED);
+
+        taData.draw(PIN_2, PIN_2, PIN_2);
+
+        taData.concealedKan(WHITE, WHITE, WHITE, WHITE);
+
+        taData.draw(GREEN, GREEN);
+
+        taData.draw(PIN_5, PIN_5);
+
+        YakuAnalyzeData yaData = taData.yaData(taData.getFirstTenpai(PIN_5), PIN_5);
+
+        analyzer.analyzeYaku(yaData, taData.yakuManager);
+
+        assertTrue(taData.bothContainYaku(Yaku.LITTLE_THREE_DRAGONS));
+    }
+
+}

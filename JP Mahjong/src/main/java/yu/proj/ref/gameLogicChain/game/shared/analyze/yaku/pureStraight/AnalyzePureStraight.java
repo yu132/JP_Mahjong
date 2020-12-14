329c26f129335e589684bfea098720987dc36035
yu132 <876633022@qq.com>
2020-12-14 14:38:56 +0800

    一气通贯的分析逻辑

---
+++ JP Mahjong/src/main/java/yu/proj/ref/gameLogicChain/game/shared/analyze/yaku/pureStraight/AnalyzePureStraight.java
@@ -0,0 +1,41 @@
+package yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.pureStraight;
+
+import static yu.proj.ref.tile.TileType.*;
+
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.YakuAnalyzeData;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.YakuAnalyzer;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.YakuManager;
+import yu.proj.ref.gameLogicChain.game.shared.playerTilesManager.TilesCounterUtilForPatternAnalyze;
+import yu.proj.ref.tile.TileType;
+import yu.proj.ref.tile.Yaku;
+
+/**  
+ * @ClassName: AnalyzePureStraight  
+ *
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ *
+ * @author 余定邦  
+ *
+ * @date 2020年12月14日  
+ *  
+ */
+public class AnalyzePureStraight implements YakuAnalyzer {
+
+    @Override
+    public void analyzeYaku(YakuAnalyzeData data, YakuManager yakuManager) {
+        if (isPureStraight(data)) {
+            yakuManager.both(Yaku.PURE_STRAIGHT);
+        }
+    }
+
+    private boolean isPureStraight(YakuAnalyzeData data) {
+        TilesCounterUtilForPatternAnalyze countUtil = data.getTilesCountUtil();
+        return isPureStraight(MAN_1, MAN_4, MAN_7, countUtil) || isPureStraight(PIN_1, PIN_4, PIN_7, countUtil)
+            || isPureStraight(SOU_1, SOU_4, SOU_7, countUtil);
+    }
+
+    private boolean isPureStraight(TileType _1, TileType _4, TileType _7, TilesCounterUtilForPatternAnalyze countUtil) {
+        return countUtil.hasSequence(_1) && countUtil.hasSequence(_4) && countUtil.hasSequence(_7);
+    }
+
+}
---
+++ JP Mahjong/src/test/java/yu/proj/ref/gameLogicChain/game/shared/analyze/yaku/pureStraight/TestAnalyzePureStraight.java
@@ -0,0 +1,74 @@
+package yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.pureStraight;
+
+import static org.junit.Assert.*;
+import static yu.proj.ref.tile.TileType.*;
+
+import org.junit.Test;
+
+import yu.proj.ref.gameLogicChain.game.shared.analyze.TestAnalyzeData;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.NoYaku;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.YakuAnalyzeData;
+import yu.proj.ref.gameLogicChain.game.shared.analyze.yaku.YakuAnalyzer;
+import yu.proj.ref.tile.Yaku;
+
+/**  
+ * @ClassName: TestAnalyzePureStraight  
+ *
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ *
+ * @author 余定邦  
+ *
+ * @date 2020年12月14日  
+ *  
+ */
+public class TestAnalyzePureStraight {
+
+    private TestAnalyzeData taData = new TestAnalyzeData();
+
+    private YakuAnalyzer analyzer = new AnalyzePureStraight();
+
+    @Test
+    public void noYaku() {
+        new NoYaku().test(analyzer);
+    }
+
+    @Test
+    public void test() {
+        taData.draw(MAN_1, MAN_2, MAN_3);
+
+        taData.draw(MAN_4, MAN_5, MAN_6);
+
+        taData.draw(MAN_7, MAN_8, MAN_9);
+
+        taData.draw(NORTH, NORTH);
+
+        taData.draw(RED, RED);
+
+        YakuAnalyzeData yaData = taData.yaData(taData.getFirstTenpai(RED), RED);
+
+        analyzer.analyzeYaku(yaData, taData.yakuManager);
+
+        assertTrue(taData.bothContainYaku(Yaku.PURE_STRAIGHT));
+    }
+
+    @Test
+    public void test2() {
+        taData.draw(SOU_1, SOU_2, SOU_3);
+
+        taData.draw(SOU_4, SOU_5, SOU_6);
+
+        taData.draw(SOU_7, SOU_8, SOU_9);
+
+        taData.draw(NORTH, NORTH);
+
+        taData.draw(RED, RED);
+
+        YakuAnalyzeData yaData = taData.yaData(taData.getFirstTenpai(RED), RED);
+
+        analyzer.analyzeYaku(yaData, taData.yakuManager);
+
+        assertTrue(taData.bothContainYaku(Yaku.PURE_STRAIGHT));
+    }
+
+
+}

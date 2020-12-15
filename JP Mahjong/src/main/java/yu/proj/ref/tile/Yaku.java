83c76e067f2bdd0c14e838041802fc56445676dd
yu132 <876633022@qq.com>
2020-12-15 17:27:47 +0800

    调整Yaku的格式

--- JP Mahjong/src/main/java/yu/proj/ref/tile/Yaku.java
+++ JP Mahjong/src/main/java/yu/proj/ref/tile/Yaku.java
@@ -18,8 +18,11 @@ import yu.proj.jpmahjong.rule.Rule;
 @AllArgsConstructor
 public enum Yaku {
 
+    //@formatter:off
+    
     // [1番]
     RIICHI(1, 0, 0, 0), // 立直
+    
     DANYAO(1, DEPENDING_TO_RULE(), 0, 0) {
         @Override
         protected int hanWhenMakeCallDependingToRule(Rule rule) {
@@ -30,68 +33,128 @@ public enum Yaku {
             }
         }
     }, // 断幺九
+    
     FULLY_CONCEALED_HAND(1, 0, 0, 0), // 门清自摸
-    PREVALENT_WIND_E(ALL_1_HAN()), PREVALENT_WIND_S(ALL_1_HAN()), PREVALENT_WIND_W(ALL_1_HAN()),
+    
+    PREVALENT_WIND_E(ALL_1_HAN()), 
+    PREVALENT_WIND_S(ALL_1_HAN()),
+    PREVALENT_WIND_W(ALL_1_HAN()),
     PREVALENT_WIND_N(ALL_1_HAN()), // 场风
-    SEAT_WIND_E(ALL_1_HAN()), SEAT_WIND_S(ALL_1_HAN()), SEAT_WIND_W(ALL_1_HAN()), SEAT_WIND_N(ALL_1_HAN()), // 自风
-    DRAGON_WH(ALL_1_HAN()), DRAGON_G(ALL_1_HAN()), DRAGON_R(ALL_1_HAN()), // 三元牌
+    
+    SEAT_WIND_E(ALL_1_HAN()), 
+    SEAT_WIND_S(ALL_1_HAN()), 
+    SEAT_WIND_W(ALL_1_HAN()), 
+    SEAT_WIND_N(ALL_1_HAN()), // 自风
+    
+    DRAGON_WH(ALL_1_HAN()), 
+    DRAGON_G(ALL_1_HAN()), 
+    DRAGON_R(ALL_1_HAN()), // 三元牌
+    
     PINFU(1, 0, 0, 0), // 平和
+    
     PURE_DOUBLE_SEQUENCE(1, 0, 0, 0), // 一杯口
+    
     ROBBING_A_KAN(ALL_1_HAN()), // 抢杠
+    
     AFTER_A_KAN(ALL_1_HAN()), // 岭上开花
+    
     UNDER_THE_SEA(ALL_1_HAN()), // 海底摸月
+    
     UNDER_THE_RIVER(ALL_1_HAN()), // 河底捞鱼
+    
     LPPATSU(1, 0, 0, 0), // 一发
+    
     DORA(ALL_1_HAN()), // 宝牌
     RED_FIVE(ALL_1_HAN()), // 赤宝牌
     KITA(ALL_1_HAN()), // 北宝牌
     URA_DORA(1, 0, 0, 0), // 里宝牌
 
+    
+    
     // [2番]
     DOUBLE_RIICHI(2, 0, 0, 0), // 两立直
+    
     TRIPLE_TRIPLETS(ALL_2_HAN()), // 三色同刻
+    
     THREE_QUADS(ALL_2_HAN()), // 三杠子
+    
     ALL_TRIPLETS(ALL_2_HAN()), // 对对和
+    
     THREE_CONCEALED_TRIPLETS(2, 0, 0, 0), // 三暗刻
+    
     LITTLE_THREE_DRAGONS(ALL_2_HAN()), // 小三元
+    
     ALL_TERMINALS_AND_HONORS(ALL_2_HAN()), // 混老头
+    
     SEVEN_PAIRS(2, 0, 0, 0), // 七对子
+    
     HALF_OUTSIDE_HAND(2, 1, 0, 0), // 混全带幺九
+    
     PURE_STRAIGHT(2, 1, 0, 0), // 一气通贯
+    
     MIXED_TRIPLE_SEQUENCE(2, 1, 0, 0), // 三色同顺
 
+    
+    
     // [3番]
     TWICE_PURE_DOUBLE_SEQUENCE(3, 0, 0, 0), // 二杯口
+    
     FULL_OUTSIDE_HAND(3, 2, 0, 0), // 纯全带幺九
+    
     HALF_FLUSH(3, 2, 0, 0), // 混一色
 
+    
+    
     // [6番]
     FULL_FLUSH(6, 5, 0, 0), // 清一色
 
+    
+    
     // [满贯]
     MANGAN_AT_DRAW(5, 5, 0, 0), // 流局满贯
 
+    
+    
     // [役满]
     BLESSING_OF_HEAVEN(ALL_1_YAKUMAN()), // 天和
+    
     BLESSING_OF_EARTH(ALL_1_YAKUMAN()), // 地和
+    
     BIG_THREE_DRAGONS(ALL_1_YAKUMAN()), // 大三元
+    
     FOUR_CONCEALED_TRIPLETS(0, 0, 1, 0), // 四暗刻
+    
     ALL_HONORS(ALL_1_YAKUMAN()), // 字一色
+    
     ALL_GREENS(ALL_1_YAKUMAN()), // 绿一色
+    
     ALL_TERMINALS(ALL_1_YAKUMAN()), // 清老头
+    
     THIRTEEN_ORPHANS(ALL_1_YAKUMAN()), // 国士无双
+    
     FOUR_LITTLE_WINDS(ALL_1_YAKUMAN()), // 小四喜
+    
     FOUR_QUADS(ALL_1_YAKUMAN()), // 四杠子
+    
     NINE_GATES(ALL_1_YAKUMAN()), // 九莲宝灯
 
+    
+    
     // [双倍役满]
     SINGLE_WAIT_FOUR_CONCEALED_TRIPLETS(ALL_2_YAKUMAN()), // 四暗刻单骑
+    
     THIRTEEN_WAIT_THIRTEEN_ORPHANS(ALL_2_YAKUMAN()), // 国士无双十三面
+    
     TRUE_NINE_GATES(ALL_2_YAKUMAN()), // 纯正九莲宝灯
+    
     FOUR_BIG_WINDS(ALL_2_YAKUMAN()), // 大四喜
 
+    
+    
     // [古役和地方役]
     TRUE_ALL_GREENS(ALL_2_YAKUMAN()); // 纯正绿一色
+
+    //@formatter:on
 
     public final static int DEPENDING_TO_RULE = -1;
 
